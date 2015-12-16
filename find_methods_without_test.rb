9abd1bec1a0ea22b4782819cf255755a50912a88
Kuniaki IGARASHI <igaiga@gmail.com>
2015-12-17 08:33:49 +0900

    Refectering

--- find_methods_without_test.rb
+++ find_methods_without_test.rb
@@ -3,21 +3,17 @@ require "awesome_print"
 class MethodFinder
   RUBY_DIR = "/Users/igarashi/github_repos/github.com/ruby/ruby"
   RUBY_TEST_PATH = "test/ruby"
+  attr_accessor :class_name
 
-  def initialize
-    @methods_by_file_name = methods_by_file_name
-  end
-
-  def without_test_methods
-    @methods_by_file_name
+  def initialize(class_name: nil)
+    @class_name = class_name
   end
 
   def methods_by_instance
-
   end
 
-  def methods_by_file_name
-    file_array = "test_array.rb"
+  def methods_by_file_name()
+    file_array = "test_#{@class_name.downcase}.rb"
     file = File.join(RUBY_DIR, RUBY_TEST_PATH, file_array)
     methods = []
     File.open(file, "r") do |f|
@@ -27,8 +23,12 @@ class MethodFinder
     end
     target_test_methods = methods.map{ |x| x =~ /^test_(.+)\z/ ? $1 : nil }.compact.sort
   end
+
+  def run
+    @methods_by_file_name = methods_by_file_name
+  end
 end
 
-ap MethodFinder.new.without_test_methods
+ap MethodFinder.new(class_name: "Array").run
 # TODO Array.new.instance_methods(false)と比較
 # TODO 全該当ファイル
